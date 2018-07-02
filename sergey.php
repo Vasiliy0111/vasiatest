65c0185cb846674280a7b317d01f5e3b9d6df8e0
Shakhov Sergey <shakhovsergey@mail.ru>
2018-07-03 00:13:40 +0300

    change in sergey.php

--- sergey.php
+++ sergey.php
@@ -1,15 +1,3 @@
 <?php
 echo "How are you being";
 echo "what you been up to";
-echo "up to 70 meters";
-echo "up to 80 meters";
-echo "up to 100 meters";
-echo "Hello";
-echo "Hisdd";
-<<<<<<< HEAD
-echo "Hi man";
-echo "How is it going?";
-=======
-echo "Hi buddy";
->>>>>>> fix1
-?>
